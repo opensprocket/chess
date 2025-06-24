641e425137ec36b7b2a2a441bf3fa3b3c248d94e
opensprocket <opensprocket@users.noreply.github.com>
2025-06-24 13:20:37 -0600

    add resetBoard()

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -7,9 +7,11 @@ package chess;
  * signature of the existing methods.
  */
 public class ChessBoard {
-    private ChessPiece[][] squares = new ChessPiece[8][8];
+
+    private ChessPiece[][] squares;
+
     public ChessBoard() {
-        
+        squares = new ChessPiece[8][8];
     }
 
 
@@ -39,6 +41,37 @@ public class ChessBoard {
      * (How the game of chess normally starts)
      */
     public void resetBoard() {
-        throw new RuntimeException("Not implemented");
+        squares = new ChessPiece[8][8];
+
+        // add white team
+        // add nobility
+        addPiece(new ChessPosition(1,1), new ChessPiece(ChessGame.TeamColor.WHITE), ChessPiece.PieceType.ROOK);
+        addPiece(new ChessPosition(1,2), new ChessPiece(ChessGame.TeamColor.WHITE), ChessPiece.PieceType.KNIGHT);
+        addPiece(new ChessPosition(1,3), new ChessPiece(ChessGame.TeamColor.WHITE), ChessPiece.PieceType.BISHOP);
+        addPiece(new ChessPosition(1,4), new ChessPiece(ChessGame.TeamColor.WHITE), ChessPiece.PieceType.QUEEN);
+        addPiece(new ChessPosition(1,5), new ChessPiece(ChessGame.TeamColor.WHITE), ChessPiece.PieceType.KING);
+        addPiece(new ChessPosition(1,6), new ChessPiece(ChessGame.TeamColor.WHITE), ChessPiece.PieceType.BISHOP);
+        addPiece(new ChessPosition(1,7), new ChessPiece(ChessGame.TeamColor.WHITE), ChessPiece.PieceType.KNIGHT);
+        addPiece(new ChessPosition(1,8), new ChessPiece(ChessGame.TeamColor.WHITE), ChessPiece.PieceType.ROOK);
+        // add pawns
+        for (int i = 1; i < 9; i++) {
+            addPiece(new ChessPosition(2,i), new ChessPiece(ChessGame.TeamColor.WHITE), ChessPiece.PieceType.PAWN);
+        }
+
+        // add black team
+        // add nobility
+        addPiece(new ChessPosition(8,1), new ChessPiece(ChessGame.TeamColor.BLACK), ChessPiece.PieceType.ROOK);
+        addPiece(new ChessPosition(8,2), new ChessPiece(ChessGame.TeamColor.BLACK), ChessPiece.PieceType.KNIGHT);
+        addPiece(new ChessPosition(8,3), new ChessPiece(ChessGame.TeamColor.BLACK), ChessPiece.PieceType.BISHOP);
+        addPiece(new ChessPosition(8,4), new ChessPiece(ChessGame.TeamColor.BLACK), ChessPiece.PieceType.QUEEN);
+        addPiece(new ChessPosition(8,5), new ChessPiece(ChessGame.TeamColor.BLACK), ChessPiece.PieceType.KING);
+        addPiece(new ChessPosition(8,6), new ChessPiece(ChessGame.TeamColor.BLACK), ChessPiece.PieceType.BISHOP);
+        addPiece(new ChessPosition(8,7), new ChessPiece(ChessGame.TeamColor.BLACK), ChessPiece.PieceType.KNIGHT);
+        addPiece(new ChessPosition(8,8), new ChessPiece(ChessGame.TeamColor.BLACK), ChessPiece.PieceType.ROOK);
+        // add pawns
+        for (int i = 1; i < 9; i++) {
+            addPiece(new ChessPosition(7,i), new ChessPiece(ChessGame.TeamColor.BLACK), ChessPiece.PieceType.PAWN);
+        }
+
     }
 }
