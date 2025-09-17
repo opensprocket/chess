e427b63a0c939d5fdf588be47316d1dfb01d8b27
opensprocket <opensprocket@users.noreply.github.com>
2025-09-16 22:58:34 -0600

    add functionality to ChessGame

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -10,6 +10,8 @@ import java.util.Collection;
  */
 public class ChessGame {
 
+    public TeamColor currentTurn = TeamColor.WHITE; // start on White turn by default
+
     public ChessGame() {
 
     }
@@ -18,7 +20,7 @@ public class ChessGame {
      * @return Which team's turn it is
      */
     public TeamColor getTeamTurn() {
-        throw new RuntimeException("Not implemented");
+        return currentTurn;
     }
 
     /**
@@ -27,7 +29,7 @@ public class ChessGame {
      * @param team the team whose turn it is
      */
     public void setTeamTurn(TeamColor team) {
-        throw new RuntimeException("Not implemented");
+        currentTurn = team;
     }
 
     /**
