4d7e7f4f86f2cf0c431bbcfa733900c7185ec336
opensprocket <opensprocket@users.noreply.github.com>
2025-10-22 13:23:29 -0600

    Create ClearServiceTest.java

---
+++ server/src/test/java/service/ClearServiceTest.java
@@ -0,0 +1,39 @@
+package service;
+
+import dataaccess.*;
+import datamodel.*;
+import org.junit.jupiter.api.*;
+
+import javax.xml.crypto.Data;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class ClearServiceTest {
+    private DataAccess dataAccess;
+    private ClearService clearService;
+
+    @BeforeEach
+    void setUp() {
+        dataAccess = new MemoryDataAccess();
+        clearService = new ClearService(dataAccess);
+    }
+
+    @Test
+    void clearApplicationData() throws DataAccessException {
+        dataAccess.createUser(new UserData("user", "pass", "email"));
+        dataAccess.createAuth("user");
+        dataAccess.createGame("testGame");
+
+        // pre
+        assertNotNull(dataAccess.getUser("user"));
+        assertFalse(dataAccess.listGames().isEmpty());
+
+        // trigger clear
+        clearService.clearApplication();
+
+        // evaluate result
+        assertNull(dataAccess.getUser("user"));
+        assertTrue(dataAccess.listGames().isEmpty());
+    }
+}
