d5f2761806e5631aae085e42376494f700215e24
opensprocket <opensprocket@users.noreply.github.com>
2025-10-22 13:44:53 -0600

    Update UserServiceTest.java

--- server/src/test/java/service/UserServiceTest.java
+++ server/src/test/java/service/UserServiceTest.java
@@ -1,31 +1,98 @@
 package service;
 
+import dataaccess.DataAccess;
+import dataaccess.DataAccessException;
 import dataaccess.MemoryDataAccess;
+import datamodel.LoginRequest;
+import datamodel.LoginResult;
 import datamodel.RegistrationResult;
 import datamodel.UserData;
+import org.eclipse.jetty.server.Authentication;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import javax.xml.crypto.Data;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class UserServiceTest {
+    private DataAccess dataAccess;
+    private UserService userService;
+    private UserData testUser;
 
-//    @Test
-//    void clear() {
-//
-//    }
-//
-//    @Test
-//    void register() {
-//        var user = new UserData("joe", "1234", "joe@joe");
-//        var authTok = "xyz";
-//
-//        var da = new MemoryDataAccess();
-//        var service = new UserService(da);
-//
-//        RegistrationResult response = service.register(user);
-//        assertNotNull(response);
-//        assertEquals(response.username(), user.username());
-//        assertNotNull(response.authToken());
-//        assertEquals();
-//    }
+    @BeforeEach
+    void setUp() {
+        dataAccess = new MemoryDataAccess();
+        userService = new UserService(dataAccess);
+        testUser = new UserData("player1", "hunter2", "player1@email.com");
+    }
+
+    @Test
+    void registerSuccessful() throws DataAccessException {
+        RegistrationResult result = userService.register(testUser);
+        assertNotNull(result);
+        assertEquals("player1", result.username());
+        assertNotNull(result.authToken());
+        assertNotNull(dataAccess.getUser("player1"));
+    }
+
+    @Test
+    void usernameAlreadyTaken() throws DataAccessException {
+        userService.register(testUser);
+        DataAccessException ex = assertThrows(DataAccessException.class, () -> userService.register(testUser));
+        assertEquals("Error: already taken", ex.getMessage());
+    }
+
+    @Test
+    void registerBadRequest() {
+        UserData badUser = new UserData("user", null, "email@address.com");
+        DataAccessException ex = assertThrows(DataAccessException.class, () -> userService.register(badUser));
+        assertEquals("Error: bad request", ex.getMessage());
+    }
+
+    @Test
+    void loginSuccess() throws DataAccessException {
+        userService.register(testUser);
+        LoginRequest req = new LoginRequest("player1", "hunter2");
+        LoginResult result = userService.login(req);
+
+        assertNotNull(result);
+        assertEquals("player1", result.username());
+        assertNotNull(result.authToken());
+    }
+
+    @Test
+    void loginUserNotFound() {
+        LoginRequest req = new LoginRequest("skeleton", "key");
+        DataAccessException ex = assertThrows(DataAccessException.class, () -> userService.login(req));
+        assertEquals("Error: Unauthorized", ex.getMessage());
+    }
+
+    @Test
+    void loginWrongPassword() throws DataAccessException {
+        userService.register(testUser);
+        LoginRequest req = new LoginRequest(testUser.username(), "not-my-password");
+        DataAccessException ex = assertThrows(DataAccessException.class, () -> userService.login(req));
+        assertEquals("Error: Unauthorized", ex.getMessage());
+    }
+
+    @Test
+    void logoutSuccess() throws DataAccessException {
+        RegistrationResult registrationResult = userService.register(testUser);
+        String authToken = registrationResult.authToken();
+
+        // pre
+        assertNotNull(dataAccess.getAuth(authToken));
+        // trigger
+        userService.logout(authToken);
+        // post
+        assertNull(dataAccess.getAuth(authToken));
+    }
+
+    @Test
+    void logoutUnauthorized() {
+        String fakeToken = "utes-r-good-at-football";
+        DataAccessException ex = assertThrows(DataAccessException.class, () -> userService.logout(fakeToken));
+        assertEquals("Error: Unauthorized", ex.getMessage());
+    }
 }
