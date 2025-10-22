a2acb6becb9ad4b735339b3a44e9d42ebff16e69
opensprocket <opensprocket@users.noreply.github.com>
2025-10-22 13:16:34 -0600

    Create ClearService.java

---
+++ server/src/main/java/service/ClearService.java
@@ -0,0 +1,16 @@
+package service;
+
+import dataaccess.DataAccess;
+import dataaccess.DataAccessException;
+
+public class ClearService {
+    private DataAccess dataAccess;
+
+    public ClearService(DataAccess dataAccess) {
+        this.dataAccess = dataAccess;
+    }
+
+    public void clearApplication() throws DataAccessException {
+        dataAccess.clear();
+    }
+}
