e760a2c0aaeb9e38a5d24833d5cbe017067152d7
opensprocket <opensprocket@users.noreply.github.com>
2025-10-22 13:19:34 -0600

    temp disable in class examples

--- server/src/test/java/dataaccess/DataAccessTest.java
+++ server/src/test/java/dataaccess/DataAccessTest.java
@@ -6,23 +6,23 @@ import org.junit.jupiter.api.*;
 
 public class DataAccessTest {
 
-    @Test
-    void clear() {
-        var user = new UserData("Joe", "john", "someone");
-        DataAccess da = new MemoryDataAccess();
-        assertNull(da.getUser(user.username()));
-        da.createUser(user);
-        assertNotNull(da.getUser(user.username()));
-
-    }
-
-    @Test
-    void createUser() {
-
-    }
-
-    @Test
-    void getUser() {
-
-    }
+//    @Test
+//    void clear() {
+//        var user = new UserData("Joe", "john", "someone");
+//        DataAccess da = new MemoryDataAccess();
+//        assertNull(da.getUser(user.username()));
+//        da.createUser(user);
+//        assertNotNull(da.getUser(user.username()));
+//
+//    }
+//
+//    @Test
+//    void createUser() {
+//
+//    }
+//
+//    @Test
+//    void getUser() {
+//
+//    }
 }
--- server/src/test/java/service/UserServiceTest.java
+++ server/src/test/java/service/UserServiceTest.java
@@ -9,23 +9,23 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class UserServiceTest {
 
-    @Test
-    void clear() {
-
-    }
-
-    @Test
-    void register() {
-        var user = new UserData("joe", "1234", "joe@joe");
-        var authTok = "xyz";
-
-        var da = new MemoryDataAccess();
-        var service = new UserService(da);
-
-        RegistrationResult response = service.register(user);
-        assertNotNull(response);
-        assertEquals(response.username(), user.username());
-        assertNotNull(response.authToken());
-        assertEquals();
-    }
+//    @Test
+//    void clear() {
+//
+//    }
+//
+//    @Test
+//    void register() {
+//        var user = new UserData("joe", "1234", "joe@joe");
+//        var authTok = "xyz";
+//
+//        var da = new MemoryDataAccess();
+//        var service = new UserService(da);
+//
+//        RegistrationResult response = service.register(user);
+//        assertNotNull(response);
+//        assertEquals(response.username(), user.username());
+//        assertNotNull(response.authToken());
+//        assertEquals();
+//    }
 }
