d63c206d0898f03c8272d71ac36f75d89bc89b98
opensprocket <opensprocket@users.noreply.github.com>
2025-10-22 13:15:42 -0600

    Update DataAccess.java

--- server/src/main/java/dataaccess/DataAccess.java
+++ server/src/main/java/dataaccess/DataAccess.java
@@ -1,13 +1,24 @@
 package dataaccess;
 
-import datamodel.UserData;
+import datamodel.*;
+
+import java.util.Collection;
 
 public interface DataAccess {
-    void saveUser(UserData user);
-    void getUser(String user);
+//    void saveUser(UserData user);
+//    void getUser(String user);
 
 
     void clear() throws DataAccessException;
     void createUser(UserData user);
-    
+    UserData getUser(String username);
+    AuthData createAuth(String username) throws DataAccessException;
+    AuthData getAuth(String authToken) throws DataAccessException;
+    void deleteAuth(String authToken) throws DataAccessException;
+
+    //game
+    int createGame(String gameName) throws DataAccessException;
+    GameData getGame(int gameID) throws DataAccessException;
+    Collection<GameData> listGames() throws DataAccessException;
+    void updateGame(int gameID, GameData game) throws DataAccessException;
 }
