13ecade08d4a8ba08f4be17835cebc299224aad3
opensprocket <opensprocket@users.noreply.github.com>
2025-10-22 19:51:14 -0600

    begin writing dataaccess tests

--- server/src/test/java/dataaccess/DataAccessTest.java
+++ server/src/test/java/dataaccess/DataAccessTest.java
@@ -1,28 +1,52 @@
 package dataaccess;
 
-import dataaccess.MemoryDataAccess;
-import datamodel.UserData;
+import datamodel.*;
 import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class DataAccessTest {
 
-//    @Test
-//    void clear() {
-//        var user = new UserData("Joe", "john", "someone");
-//        DataAccess da = new MemoryDataAccess();
-//        assertNull(da.getUser(user.username()));
-//        da.createUser(user);
-//        assertNotNull(da.getUser(user.username()));
-//
-//    }
-//
-//    @Test
-//    void createUser() {
-//
-//    }
-//
-//    @Test
-//    void getUser() {
-//
-//    }
+    private DataAccess dataAccess;
+    private UserData testUser;
+
+    @BeforeEach
+    void setUp() {
+        dataAccess = new MemoryDataAccess();
+        testUser = new UserData("player1", "pass123", "player1@email.com");
+    }
+
+    // clear
+    @Test
+    void clearSuccess() throws DataAccessException {
+        dataAccess.createUser(testUser);
+        dataAccess.createAuth(testUser.username());
+        dataAccess.createGame("test game");
+
+        assertNotNull(dataAccess.getUser(testUser.username()));
+        assertFalse(dataAccess.listGames().isEmpty());
+
+        dataAccess.clear();
+
+        assertNull(dataAccess.getUser(testUser.username()));
+        assertTrue(dataAccess.listGames().isEmpty());
+    }
+
+    @Test
+    void createUserSuccess() throws DataAccessException {
+        dataAccess.createUser(testUser);
+        UserData retrievedUser = dataAccess.getUser(testUser.username());
+        assertEquals(testUser, retrievedUser);
+    }
+
+    @Test
+    void createUserDuplicateFail() throws DataAccessException {
+        dataAccess.createUser(testUser);
+        UserData updatedUser = new UserData("player1", "hunter2", "notplayer1@email.com");
+        dataAccess.createUser(updatedUser);
+
+        UserData retrievedUser = dataAccess.getUser("player1");
+        assertEquals(updatedUser, retrievedUser);
+        assertEquals("hunter2", retrievedUser.password());
+    }
+
 }
