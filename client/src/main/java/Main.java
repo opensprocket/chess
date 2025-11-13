92cd29d5d9be1eaa41de6e020804e1c2e8f90886
opensprocket <opensprocket@users.noreply.github.com>
2025-11-12 23:06:05 -0700

    Update Main.java

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -1,8 +1,14 @@
 import chess.*;
+import client.Repl;
 
 public class Main {
     public static void main(String[] args) {
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
         System.out.println("♕ 240 Chess Client: " + piece);
+
+//        int port = 8080;
+//        var serverUrl = "http://localhost:" + port;
+
+        new Repl().run();
     }
 }
