5f5fd2b4d4968e921040fbd7bea921554db6cad6
opensprocket <opensprocket@users.noreply.github.com>
2025-11-13 19:53:37 -0700

    Update ChessClient.java

--- client/src/main/java/client/ChessClient.java
+++ client/src/main/java/client/ChessClient.java
@@ -1,12 +1,18 @@
 package client;
 
+import java.util.Arrays;
+import client.State;
+
 public class ChessClient {
 
     private final String serverUrl;
     private final ServerFacade server;
-    public ChessClient(String serverUrl, String serverUrl1, ServerFacade server) {
+    private State state;
+
+    public ChessClient(String serverUrl, ServerFacade server) {
 
         this.serverUrl = serverUrl;
         this.server = new ServerFacade(serverUrl);
+        this.state = State.SIGNED_OUT;
     }
 }
