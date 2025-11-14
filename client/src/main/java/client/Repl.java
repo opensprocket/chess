8882cd6f38cc6ca7a526bba3dc359c04f33c212f
opensprocket <opensprocket@users.noreply.github.com>
2025-11-13 20:06:38 -0700

    Update Repl.java

--- client/src/main/java/client/Repl.java
+++ client/src/main/java/client/Repl.java
@@ -12,11 +12,12 @@ public class Repl {
     public void run() {
         Scanner scanner = new Scanner(System.in);
         var result = "";
-
         boolean run = true;
 
         System.out.println(EscapeSequences.SET_TEXT_COLOR_GREEN + "Chess Client Online");
+        System.out.println(EscapeSequences.SET_TEXT_COLOR_GREEN + "Welcome to Chess 240, type Help to get started!");
         while (run) {
+            printPrompt();
             String line = scanner.nextLine();
 
             try {
@@ -25,13 +26,21 @@ public class Repl {
                     run = false;
                     continue;
                 }
+
+                result = client.eval(line);
+                System.out.print(EscapeSequences.SET_TEXT_COLOR_BLUE + result);
+
             } catch (Throwable e) {
                 String msg = e.toString();
                 System.out.print(msg);
             }
+            System.out.println();
 
-
-            System.out.println(EscapeSequences.SET_TEXT_COLOR_BLUE + "You typed: " + line);
+//            System.out.println(EscapeSequences.SET_TEXT_COLOR_BLUE + "You typed: " + line);
         }
+    }
+
+    private void printPrompt() {
+        System.out.print(EscapeSequences.RESET_TEXT_COLOR + ">>> " + EscapeSequences.SET_TEXT_COLOR_GREEN);
     }
 }
