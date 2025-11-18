4033463be65b69469082c50f50b38c5952251d18
opensprocket <opensprocket@users.noreply.github.com>
2025-11-17 19:37:14 -0700

    Update ServerFacade.java

--- client/src/main/java/client/ServerFacade.java
+++ client/src/main/java/client/ServerFacade.java
@@ -24,4 +24,52 @@ public class ServerFacade {
     public void clearDatabase() throws FacadeException {
         makeRequest("DELETE", "/db", null, null, null);
     }
+
+    private <T> T makeRequest(String method, String path, Object reqObj, String authToken, Class<T> responseClass) throws FacadeException {
+        try {
+            URI uri = new URI(serverUrl + path);
+            HttpRequest.Builder builder = HttpRequest.newBuilder(uri);
+
+            String reqBody = (reqObj == null) ? "" : gson.toJson(reqObj);
+
+            HttpRequest.BodyPublisher bodyPublisher = (reqBody.isEmpty()) ? HttpRequest.BodyPublishers.noBody() : HttpRequest.BodyPublishers.ofString(reqBody);
+
+            if (!reqBody.isEmpty()) {
+                builder.header("Cotent-Type", "application/json");
+            }
+            if (authToken != null) {
+                builder.header("Authorization", authToken);
+            }
+
+            HttpRequest req = builder.build();
+            HttpResponse<String> res = client.send(req, HttpResponse.BodyHandlers.ofString());
+
+            return handleResponse(res, responseClass);
+
+        } catch (Exception ex) {
+            throw new FacadeException(ex.getMessage());
+        }
+    }
+
+    private <T> T handleResponse(HttpResponse<String> res, Class<T> responseClass) throws FacadeException {
+        try {
+            if (res.statusCode() >= 200 && res.statusCode() < 300) {
+                if (responseClass != null && res.body() != null && !res.body().isEmpty()) {
+                    return gson.fromJson(res.body(), responseClass);
+                } else {
+                    return null;
+                }
+            } else {
+                try {
+                    ErrorResponse errRes = gson.fromJson(res.body(), ErrorResponse.class);
+                    throw new FacadeException(errRes.message());
+                } catch (Exception ex) {
+                    throw new FacadeException(String.format("Error: %d - %s", res.statusCode(), res.body()));
+                }
+            }
+        } catch (Exception ex) {
+            throw new FacadeException(ex.getMessage());
+        }
+    }
+
 }
