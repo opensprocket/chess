3a110268d5bd28ab8ad853c10346408b11543534
opensprocket <opensprocket@users.noreply.github.com>
2025-11-17 19:21:28 -0700

    Update ServerFacade.java

--- client/src/main/java/client/ServerFacade.java
+++ client/src/main/java/client/ServerFacade.java
@@ -1,8 +1,25 @@
 package client;
 
+import com.google.gson.Gson;
+
+import java.io.IOException;
+import java.net.*;
+import java.net.http.*;
+import chess.datamodel.*;
+import java.net.http.HttpRequest.*;
+
+
 public class ServerFacade {
 
+    private final String serverUrl;
+    private final HttpClient client;
+    private final Gson gson;
+
     public ServerFacade(String serverUrl) {
+        this.serverUrl = serverUrl;
+        this.client = HttpClient.newHttpClient();
+        this.gson = new Gson();
+    }
 
     }
 }
