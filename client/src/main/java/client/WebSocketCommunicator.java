70ab7d7c3a52d6ceb297ebebbb8004ed90ac1740
opensprocket <opensprocket@users.noreply.github.com>
2025-12-04 00:54:23 -0700

    helper interface

--- client/src/main/java/client/WebSocketCommunicator.java
+++ client/src/main/java/client/WebSocketCommunicator.java
@@ -104,4 +104,9 @@ public class WebSocketCommunicator {
         }
     }
 
+    public interface NotificationHandler {
+        void onLoadGame(ChessGame game);
+        void onError(String errorMessage);
+        void onNotification(String message);
+    }
 }
